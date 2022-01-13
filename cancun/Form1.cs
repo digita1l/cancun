fbc84e57a781553282cde62884b41300a51de3d7
digital <Grigostt@yandex.ru>
2022-01-14 02:08:02 +0700

    Добавьте файлы проекта.

---
+++ cancun/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cancun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double a;
+        double b;
+        double c;
+        double d;
+        double v;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            c = Convert.ToDouble(textBox3.Text);
+            d = Convert.ToDouble(textBox4.Text);
+            
+            
+
+            v = a + b + c + d;
+            v = a + b;
+            v = a + b + c;
+            
+        }
+    }
+}
---
+++ cancun/fb.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cancun
+{
+    class fb
+    {
+        public double v;
+
+     
+
+        public void Addt(ref double a,
+        ref double b,
+        ref double c,
+        ref double d,
+        ref double v)
+        {
+            v = v;
+            a = a;
+            d = b;
+            d = d;
+            v=a + b + c + d;
+            v = a + b;
+            v = a + b + c;
+          
+
+        }
+        public void Addt(ref double t, ref double a,
+        ref double b,
+        ref double c,
+        ref double d,
+        ref double v)
+        {
+            t = v;
+            v = a + b + c + d;
+            v = a + b;
+            v = a + b + c;
+            
+        }
+    }
+
+}
